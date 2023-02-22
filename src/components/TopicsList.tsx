e5c6995debeca93ba8eca953e98778d10791e1cc
Isaac Pontes <isaaclfp@outlook.com>
2023-02-21 21:46:43 -0300

    project finished

---
+++ src/components/TopicsList.tsx
@@ -0,0 +1,34 @@
+import { Dispatch, SetStateAction } from "react"
+import { Topic } from "./Home"
+
+type TopicsListProps = {
+  topics: Topic[]
+  setTopics: Dispatch<SetStateAction<Topic[]>>
+  setOpenTopic: Dispatch<SetStateAction<Topic | null>>
+}
+
+export default function TopicsList({ topics, setTopics, setOpenTopic }: TopicsListProps) {
+
+  async function deleteTopic(id: string) {
+    await fetch(`http://localhost:3000/topics/${id}`, { method: "DELETE" })
+    const updatedTopics = topics.filter(t => t._id !== id)
+    setTopics(updatedTopics)
+  }
+
+  return (
+    <main id="topics">
+      {topics.length === 0
+        ? <h3>Parece que não tem nada aqui... 😕</h3>
+        : topics.map(topic => (
+          <div className="topic">
+            <h2>{topic.title}</h2>
+            <div>
+              <button onClick={() => setOpenTopic(topic)}>Entrar na sala</button>
+              <button onClick={() => deleteTopic(topic._id)}>Excluir</button>
+            </div>
+          </div>
+        ))
+      }
+    </main>
+  )
+}
